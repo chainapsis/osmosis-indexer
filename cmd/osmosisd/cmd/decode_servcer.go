cbaa733caf31f3d82e12dd7a5de53628a20a2ea0
lkh1434 <lkh1434@gmail.com>
2024-11-19 13:43:35 +0900

    add querier

---
+++ cmd/osmosisd/cmd/decode_servcer.go
@@ -0,0 +1,42 @@
+package cmd
+
+// DONTCOVER
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/osmosis-labs/osmosis/v27/app"
+	"github.com/osmosis-labs/osmosis/v27/cmd/osmosisd/cmd/decoder"
+)
+
+const (
+	decodeServerPort = "decodeServerPort"
+)
+
+// decoderCmd gets cmd to run decode server
+func decoderCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "decoder",
+		Short: "Example osmosisd decode -p 8888, which would run decoder server on specified port",
+		Long:  "decoder command runs decoder server to decode byte array to General Cosmos messages",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			decodeServerFlag, err := cmd.Flags().GetString(decodeServerPort)
+			if err != nil {
+				return err
+			}
+			d := decoder.Decoder{
+				EncodingConfig: app.MakeEncodingConfig(),
+			}
+			err = d.ListenAndServe(decodeServerFlag)
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+			return nil
+		},
+	}
+	cmd.Flags().StringP(decodeServerPort, "p", "", "port to listen to")
+	cmd.MarkFlagRequired(decodeServerPort)
+	return cmd
+}
